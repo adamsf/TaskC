12ddbfdfc43be01f5c2b93298ab9cf7ae315f6d4
Francis Adams <AdamsFA19@pwcs-edu.org>
2019-01-08 13:12:45 -0500

    pls work

---
+++ manipulation.hs
@@ -0,0 +1,23 @@
+manip :: String -> Integer -> String
+
+manip as b
+	 
+	|even b = as
+	 
+	|otherwise = reverse as 
+
+
+
+boustro :: [String] -> [String]
+
+boustro as = [manip (fst x) (snd x) | x <- zip as [1..]] 
+
+
+
+chunks :: String -> [String]
+
+chunks as 
+        
+	| length as < 20 = [as]
+     
+	| otherwise = (take 20 as):(chunks (drop 20 as))
