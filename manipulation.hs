53fba0390b7f95d13dde8e3fdb33c774bdb4979b
Francis Adams <AdamsFA19@pwcs-edu.org>
2019-01-08 10:10:34 -0500

    added chonk method

--- manipulation.hs
+++ manipulation.hs
@@ -7,3 +7,10 @@ boustro :: [String] -> [String]
 
 boustro as = [manip (fst x) (snd x) | x <- zip as [1..]]   
 
+chunks :: String -> [String]
+
+chunks as 
+        
+	| length as < 20 = [as]
+
+        | otherwise = (take 20 as):(chunks (drop 20 as))
